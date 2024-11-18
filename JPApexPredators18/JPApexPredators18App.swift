5f320c14572ff883f0f815fefb6029150c48d6d6
slackerkids <145984578+slackerkids@users.noreply.github.com>
2024-11-18 10:31:43 +0500

    Initial Commit

---
+++ JPApexPredators18/JPApexPredators18App.swift
@@ -0,0 +1,17 @@
+//
+//  JPApexPredators18App.swift
+//  JPApexPredators18
+//
+//  Created by Sagidanov Bakhtiyar on 18.11.2024.
+//
+
+import SwiftUI
+
+@main
+struct JPApexPredators18App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
