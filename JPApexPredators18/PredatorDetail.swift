0269ace98c062c24f9ec73fff3c69971eb5bd2eb
slackerkids <145984578+slackerkids@users.noreply.github.com>
2024-11-26 14:35:34 +0500

    Predator detail start

---
+++ JPApexPredators18/PredatorDetail.swift
@@ -0,0 +1,37 @@
+//
+//  PredatorDetail.swift
+//  JPApexPredators18
+//
+//  Created by Sagidanov Bakhtiyar on 26.11.2024.
+//
+
+import SwiftUI
+
+struct PredatorDetail: View {
+    let predator: ApexPredator
+    
+    var body: some View {
+        ScrollView {
+            ZStack {
+                // Background Image
+                
+                // Dino Image
+            }
+            
+            // Name
+            
+            // Current Location
+            
+            // Appears In
+            
+            // Movie Moments
+            
+            // Link to Webpage
+        }
+    }
+}
+
+#Preview {
+    PredatorDetail(predator: Predators().apexPredators[2])
+//        .preferredColorScheme(.dark)
+}
