92f5c8dac41793ae5f0f3e2c8402395315316ee7
slackerkids <145984578+slackerkids@users.noreply.github.com>
2024-11-27 12:42:13 +0500

    Predator detail

--- JPApexPredators18/PredatorDetail.swift
+++ JPApexPredators18/PredatorDetail.swift
@@ -29,15 +29,47 @@ struct PredatorDetail: View {
                         .offset(y:20)
                 }
                 
-                // Name
-                
-                // Current Location
-                
-                // Appears In
-                
-                // Movie Moments
-                
-                // Link to Webpage
+                VStack(alignment: .leading) {
+                    // Name
+                    Text(predator.name)
+                        .font(.largeTitle)
+                    
+                    // Current Location
+                    
+                    // Appears In
+                    Text("Appears In:")
+                        .font(.title3)
+                    
+                    ForEach(predator.movies, id: \.self) { movie in
+                        Text("•" + movie)
+                            .font(.subheadline)
+                    }
+                    
+                    // Movie Moments
+                    Text("Movie Moments")
+                        .font(.title)
+                        .padding(.top, 15)
+                    
+                    ForEach(predator.movieScenes) { scene in
+                        Text(scene.movie)
+                            .font(.title2)
+                            .padding(.vertical, 1)
+                        
+                        Text(scene.sceneDescription)
+                            .padding(.bottom, 15)
+                    }
+                    
+                    // Link to Webpage
+                    Text("Read More:")
+                        .font(.caption)
+                    
+                    Link(predator.link, destination: URL(string: predator.link)!)
+                        .font(.caption)
+                        .foregroundStyle(.blue)
+                }
+                .padding()
+                .padding(.bottom)
+                .frame(width: geo.size.width, alignment: .leading)
             }
             .ignoresSafeArea()
         }
